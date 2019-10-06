a74c5d44e874cd651ae9e7f9dde4b964e9ddf5ef
Avinash <avinashcegian95@gmail.com>
2019-10-06 13:23:53 -0700

    Fixed the NaN error

--- Assignment-1/Code/Feature_Selection.py
+++ Assignment-1/Code/Feature_Selection.py
@@ -4,9 +4,13 @@ from sklearn.decomposition import PCA
 
 # extracted_feature = pd.read_csv("./../Extracted_Features.csv")
 extracted_feature = pd.read_csv("/Users/avinash/Code/DM-Project/Assignment-1/Extracted_Features.csv")
-extracted_feature.fillna(0)
-features = extracted_feature.drop(extracted_feature.columns[0], axis=1).values
+# extracted_feature.fillna(0)
 
+for i in range(31):
+  extracted_feature = extracted_feature.drop(extracted_feature.columns[0], axis=1)
+
+features = extracted_feature.fillna(0).values
+print(features.shape)
 # Normalizing the features value to fit the PCA
 features = StandardScaler().fit_transform(features)
 
@@ -17,6 +21,6 @@ principal_components = pca.fit_transform(features)
 
 columns = ['principal_components_'+str(i+1) for i in range(5)]
 
-final_pca = pd.Dataframe(data=principal_components, columns=columns)
+final_pca = pd.DataFrame(data=principal_components, columns=columns)
 
-final_pca.toCSV('./../principal_components_features.csv')
+final_pca.to_csv('./../principal_components_features.csv')
