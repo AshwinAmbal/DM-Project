db2097326603f1e4f890cd01a050099a665a1b97
Avinash <avinashcegian95@gmail.com>
2019-10-08 22:37:32 -0700

    ¯\_(๑❛ᴗ❛๑)_/¯

--- Assignment-1/Code/Feature_Selection.py
+++ Assignment-1/Code/Feature_Selection.py
@@ -2,30 +2,35 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.decomposition import PCA
 
-# extracted_feature = pd.read_csv("./../Extracted_Features.csv")
+
 extracted_feature = pd.read_csv("/Users/avinash/Code/DM-Project/Assignment-1/Extracted_Features.csv")
-# extracted_feature.fillna(0)
 
 for i in range(36):
   extracted_feature = extracted_feature.drop(extracted_feature.columns[0], axis=1)
 
 # print(extracted_feature.columns)
 features = extracted_feature.fillna(0)
-print(features.shape)
+print('The shape of Feature Matrix: ', features.shape)
 # Normalizing the features value to fit the PCA
 features = MinMaxScaler().fit_transform(features)
 
-
+# Creating a PCA Class to select top 5 components
 pca = PCA(n_components=5)
 
+# Executing the PCA calcualtion
 principal_components = pca.fit_transform(features)
 
 columns = ['principal_components_'+str(i+1) for i in range(5)]
 
+# Creating the Matrix of shape (5 x 33)
+# Rows: Principal Components selected by the PCA
+# Columns: Features contribution to each PC
+
 final_pca = pd.DataFrame(pca.components_,columns=extracted_feature.columns,index = ['PC-1','PC-2', 'PC-3', 'PC-4', 'PC-5'])
 
-print(final_pca.abs().sum().nlargest(38))
+# Finding the top 5 components
+print(final_pca.abs().sum().nlargest(5))
 
-# final_pca = pd.DataFrame(data=principal_components, columns=columns)
 
+# Storing the final Principal Component Matrix
 final_pca.to_csv('./../pc_matrix.csv')
